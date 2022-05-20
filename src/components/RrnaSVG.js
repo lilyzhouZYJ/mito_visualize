12712f410d797ca4f0ef7b2ed400d0e242487c3e
lilyzhouZYJ <lzhouzyj@gmail.com>
2022-05-20 21:24:22 +0000

    Update citation info

--- src/components/RrnaSVG.js
+++ src/components/RrnaSVG.js
@@ -725,7 +725,15 @@ class RrnaSVG extends React.Component{
                             <button id='download-btn' onClick={this.handleClick}>Download Image</button>
                         </div>                            
 
-                        <p id="citation-note">If you use MitoVisualize, please cite "Lake NJ, Zhou L, Xu J, Lek M. 2021. MitoVisualize: A resource for analysis of variants in human mitochondrial RNAs and DNA. bioRxiv <a href="https://www.biorxiv.org/content/10.1101/2021.12.04.470997v1" target="_blank">doi: 10.1101/2021.12.04.470997</a>".</p>
+                        <p id="citation-note">
+                            If you use MitoVisualize, please cite
+                            "Lake NJ, Zhou L, Xu J, Lek M. MitoVisualize: a resource for analysis of variants 
+                            in human mitochondrial RNAs and DNA. Bioinformatics. 2022 May 13;38(10):2967-2969.&nbsp;
+                            <a href="https://academic.oup.com/bioinformatics/article-abstract/38/10/2967/6585389" target="_blank">
+                                doi: 10.1093/bioinformatics/btac216
+                            </a>.
+                            PMID: 35561159."
+                        </p>
                     </div>
                 </div>    
                 <div id="right-container">
